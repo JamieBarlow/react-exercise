dd904caa5e89f87182fdccb3a667e67bbcef5fd8
Jamie Barlow <jamiebarlow@gmail.com>
2024-12-07 16:53:47 +0000

    Fetch and display data in list

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,23 +1,41 @@
 "use client";
 
-import { Text } from "@cruk/cruk-react-components";
+import { Text, Box } from "@cruk/cruk-react-components";
 import { NasaResponse, NasaSearchParams } from "../types";
 import { urlNasaSearch } from "../services/nasa";
 import { useQuery } from "@tanstack/react-query";
 
 export function List({ values }: { values: NasaSearchParams }) {
-  const urlNasaSearchUrl = values
-    ? urlNasaSearch(values as NasaSearchParams)
-    : "";
+  const urlNasaSearchUrl = values ? urlNasaSearch(values) : "";
 
   console.log(urlNasaSearchUrl);
 
-  const { data } = useQuery<NasaResponse>(
+  const { data, isLoading, isError, error } = useQuery<NasaResponse>(
     ["nasaSearch", values],
     () => fetch(urlNasaSearchUrl).then((res) => res.json()),
     { enabled: !!urlNasaSearchUrl.length }
   );
+  if (isLoading) {
+    return <Text>Loading...</Text>;
+  }
+  if (isError) {
+    return <Text>Error: {error?.message || "Something went wrong"}</Text>;
+  }
 
-  // TODO somehow render results
-  return <>{!!data && <Text>{JSON.stringify(data)}</Text>}</>;
+  return (
+    <Box marginTop="s" paddingTop="s">
+      {data.collection.items.map((item) =>
+        item.data.map((dataItem) => (
+          <Box key={dataItem.nasa_id} marginBottom="m">
+            <Text>{dataItem.center}</Text>
+            <Text>{dataItem.title}</Text>
+            <Text>{dataItem.location}</Text>
+            <Text>{dataItem.nasa_id}</Text>
+            <Text>{dataItem.description}</Text>
+            <Text>{dataItem.date_created}</Text>
+          </Box>
+        ))
+      )}
+    </Box>
+  );
 }
